776b91edf503503595c7f931f51a0db3427c9571
FLTravieso <frankltp9412@gmail.com>
2021-06-15 22:47:53 -0300

    firebase/facebook setup and deleting storyboard references

--- PreInterCorp/SceneDelegate.swift
+++ PreInterCorp/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +17,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+            let navigationController = UINavigationController(rootViewController: LoginViewController())
+            navigationController.navigationBar.isHidden = true
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = navigationController
+            window.backgroundColor = .white
+            window.makeKeyAndVisible()
+            self.window = window
+    }
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+
+        ApplicationDelegate.shared.application(
+            UIApplication.shared,
+            open: url,
+            sourceApplication: nil,
+            annotation: [UIApplication.OpenURLOptionsKey.annotation]
+        )
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
